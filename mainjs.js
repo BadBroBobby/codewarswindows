1cf0666cec5e0451220fbaf8c89770f6356172d0
Chris Vandborg <cvand15@student.sdu.dk>
2022-09-17 21:34:09 +0200

    Added first codewars solution

--- mainjs.js
+++ mainjs.js
@@ -1,1 +1,16 @@
 //Test
+
+//8 kyu Find the Remainder
+
+
+function remainder(n, m){
+    // Divide the larger argument by the smaller argument and return the remainder
+    return n > m ? n % m : m % n
+    
+  }
+
+  //Arrow function
+
+  const remainder = (n, m) => {
+    n > m ? n % m : m % n
+  }
