5846965190f5ba69a29fb460598deaf2f48baade
Chris Vandborg <cvand15@student.sdu.dk>
2022-09-22 21:46:02 +0200

    added anotha one

--- mainjs.js
+++ mainjs.js
@@ -41,3 +41,26 @@ function exponent(a,b){
 function subt(a,b){
     return a-b
 }
+
+/* Rock Paper Scissors
+Let's play! You have to return which player won! In case of a draw return Draw!. */
+
+//solution
+
+const rps = (p1, p2) => {
+    if(p1 === p2){
+      return 'Draw!'
+    } else if(p1 == 'scissors' && p2 == 'paper'){
+      return 'Player 1 won!'
+    } else if (p1 == 'scissors' && p2 == 'rock'){
+      return 'Player 2 won!'
+    } else if(p1 == 'rock' && p2 == 'scissors'){
+      return 'Player 1 won!'
+    } else if (p1 == 'rock' && p2 == 'paper'){
+      return 'Player 2 won!'
+    } else if(p1 == 'paper' && p2 == 'rock'){
+      return 'Player 1 won!'
+    } else if (p1 == 'paper' && p2 == 'scissors'){
+      return 'Player 2 won!'
+    }
+  };
