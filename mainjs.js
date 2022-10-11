1f04314da814e1bd2ef040034fe6a33f8aef53df
Chris Vandborg <cvand15@student.sdu.dk>
2022-10-11 13:49:28 +0200

    anotha

--- mainjs.js
+++ mainjs.js
@@ -185,3 +185,29 @@ function pickIt(arr){
   
   return [odd,even];
 }
+
+/* Given two numbers and an arithmetic operator (the name of it, as a string), return the result of the two numbers having that operator used on them.
+a and b will both be positive integers, and a will always be the first number in the operation, and b always the second.*/
+
+//solution
+function arithmetic(a, b, operator){
+  //your code here!
+  switch(operator){
+      case "add":
+       operator = "+"
+        break;
+      case "subtract":
+        operator = "-"
+        break;
+      case "multiply":
+        operator = "*"
+        break;
+      case "divide":
+        operator = "/"
+        break;
+  }
+  
+  let string = a + operator + b
+  
+  return eval(string)
+}
