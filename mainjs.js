0932f1bc869bea01218fe995f703c20fe77d175f
Chris Vandborg <cvand15@student.sdu.dk>
2022-12-14 22:02:02 +0100

    First commit. Will update daily from now on.

--- mainjs.js
+++ mainjs.js
@@ -260,3 +260,9 @@ function calculateTip(amount, rating) {
     }
   }
   
+//Task
+
+/*   Create a function named combineNames that accepts two parameters (first and last name). The function should return the full name. */
+
+//Solution
+const combineNames = (name, surname) => `${name} ${surname}`
