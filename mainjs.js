5534b88cea653d4d666666e8947a1e65061ca92a
Chris Vandborg <cvand15@student.sdu.dk>
2023-03-20 11:53:32 +0100

    8kyu for loops

--- mainjs.js
+++ mainjs.js
@@ -801,3 +801,19 @@ function getASCII(c){
   // ...
   return c.charCodeAt()
 }
+
+//8 kyu
+//Leonardo Dicaprio and Oscars
+
+//Solution
+function leo(oscar){
+  if(oscar === 88){
+    return "Leo finally won the oscar! Leo is happy"
+  }else if(oscar === 86){
+    return "Not even for Wolf of wallstreet?!"
+  }else if(oscar < 88){
+    return "When will you give Leo an Oscar?"
+  }else{
+    return "Leo got one already!"
+  }
+}
